beb9b35343f07ffc8cdc8d8b31b1feade0c2c54e
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-20 14:55:09 +0900

    feat: メールアドレス（確認）項目の必須入力条件を変更 #21

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,8 +8,21 @@ class Employee < ApplicationRecord
   validates :first_name, presence: true
   validates :account, presence: true, uniqueness: true
   validates :password, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :date_of_joining, presence: true
+
+  with_options if: :create_or_update_email? do
+    validates :email, confirmation: { case_sensitive: true }
+  end
 
   scope :active, -> {
     where(deleted_at: nil)
   }
+
+  def create_or_update_email?
+    return true if !!!email_in_database.present?
+    return true if will_save_change_to_email?
+    return false
+  end
+
 end
