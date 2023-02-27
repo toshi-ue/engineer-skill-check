d4e8b62001aa93a40b985ecae54ef4de78379437
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-28 04:20:01 +0900

    feat: articleモデルにバリデーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@
 
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'Employee', foreign_key: :author
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
