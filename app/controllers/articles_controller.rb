03a94119029c47dc03f54bd6cb16caea38621b82
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-28 10:27:55 +0900

    fix: typoを修正

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  before_action :set_artcle, only: %i[edit update destroy]
+  before_action :set_article, only: %i[edit update destroy]
 
   def index
     @articles = Article.includes(:author)
