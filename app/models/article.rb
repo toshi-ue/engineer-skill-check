6bf93452c6c540fb196e5f9e3d241c1916cb1c78
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-28 10:35:41 +0900

    feat: articleモデルのバリデーションに文字数制限を追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'Employee', foreign_key: :author
 
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 1000 }
+
 end
