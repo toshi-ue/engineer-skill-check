d311fbb925059cbbc04995a6302a15b4b95c9c7e
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-03-03 10:12:39 +0900

    feat: ページネーションを実装

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,7 +5,7 @@ class EmployeesController < ApplicationController
   before_action :set_form_option, only: %i[new create edit update]
 
   def index
-    @employees = Employee.active.order("#{sort_column} #{sort_direction}")
+    @pagy, @employees = pagy(Employee.active.order("#{sort_column} #{sort_direction}"))
   end
 
   def new
